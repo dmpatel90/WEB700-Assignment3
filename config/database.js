10a6d04300eb64e11ab1e04f19d113aed5fada6c
Dev Patel <dmpatel90@myseneca.ca>
2026-07-19 19:05:32 -0400

    Remove vercel config

--- config/database.js
+++ config/database.js
@@ -1,30 +1,18 @@
 const { Sequelize } = require("sequelize");
- 
+
 const connectionString = process.env.DATABASE_URL;
- 
-if (!connectionString) {
-    throw new Error(
-        "DATABASE_URL is missing. Add it to the .env file."
-    );
-}
- 
-const sequelize = new Sequelize(connectionString, {
+
+console.log("DATABASE_URL exists:", !!connectionString);
+
+const sequelize = new Sequelize(connectionString || "", {
     dialect: "postgres",
     logging: false,
- 
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    },
- 
-    pool: {
-        max: 3,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
     }
 });
- 
+
 module.exports = sequelize;
